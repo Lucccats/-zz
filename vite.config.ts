56e8d2f0e00f2b86a069d498d80bd77931eb41b3
Lucccats <yihonluo@gmail.com>
2025-12-10 15:10:41 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // ⚠️ 极其重要：请将下面的 'arix-signature-tree' 换成你 GitHub 仓库的实际名字
-  // 如果你的仓库链接是 https://github.com/lucccats/my-project
-  // 那这里就写 base: '/my-project/'
-  base: '/zz/', 
+  // ⚠️ 这里必须和你的仓库名 "-zz" 完全一致，前后都要有斜杠
+  base: '/-zz/', 
 })
