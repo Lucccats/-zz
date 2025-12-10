69030d45d8e463e998ab7bff97e3c74443922c1a
Lucccats <yihonluo@gmail.com>
2025-12-10 17:41:02 +0800

    Delete src directory

--- src/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // ⚠️ 这里必须和你的仓库名 "-zz" 完全一致，前后都要有斜杠
-  base: '/-zz/', 
-})
